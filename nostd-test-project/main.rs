d663994c260f6f76655d729d223e3db8f4e57153
Giuliano Losa <giuliano@losa.fr>
2023-02-22 10:48:18 -0800

    add nostd-test-project

---
+++ nostd-test-project/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+
+extern "C" {
+    fn __VERIFIER_error() -> !;
+    fn __VERIFIER_assume(pred: i32);
+    fn __VERIFIER_nondet_u32() -> u32;
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    unsafe {
+        __VERIFIER_error();
+    }
+}
+
+fn nondet_u32() -> u32 {
+    unsafe {
+        __VERIFIER_nondet_u32()
+    }
+}
+
+fn error() -> ! {
+    unsafe {
+        __VERIFIER_error()
+    }
+}
+
+// no_mangle prevents renaming symbols by the compiler
+#[no_mangle]
+pub extern fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+    let x:u32 = nondet_u32();
+    // if 1 < x && x <= 2 && x*x <= 1 {
+    if 1 < x && x*x < 1 {
+    // if x*x == 4 {
+        panic!()
+    }
+    0
+}
