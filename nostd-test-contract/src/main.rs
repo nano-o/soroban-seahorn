e687355c953e72a34f32f8bbed763c797e134e5a
Giuliano Losa <giuliano@losa.fr>
2023-03-01 20:44:40 -0800

    update nostd-test-contract + submodules

--- nostd-test-contract/src/main.rs
+++ nostd-test-contract/src/main.rs
@@ -1,26 +1,8 @@
 #![no_std]
 #![no_main]
-// #![feature(core_intrinsics)] // for abort()
 use soroban_sdk::{symbol, Env, Symbol};
-use core::panic::PanicInfo;
 
-extern "C" {
-    fn __VERIFIER_error() -> !;
-    fn __VERIFIER_assume(pred: i32);
-    fn __VERIFIER_nondet_u32() -> u32;
-}
-
-// fn nondet_u32() -> u32 {
-    // unsafe {
-        // __VERIFIER_nondet_u32()
-    // }
-// }
-
-fn error() -> ! {
-    unsafe {
-        __VERIFIER_error()
-    }
-}
+// use seahorn_stubs::*;
 
 const COUNTER: Symbol = symbol!("COUNTER");
 
@@ -54,11 +36,4 @@ pub extern fn main(_argc: i32, _argv: *const *const u8) -> i32 {
     let e = Env::default();
     IncrementContract::increment(e);
     0
-}
-
-#[panic_handler]
-#[no_mangle]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    // core::intrinsics::abort();
-    error();
 }
